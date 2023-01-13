46c1a9dd84e0c5a55fcb5d0f2a0ee94cdefec8fd
eric chai <no8cai@gmail.com>
2023-01-13 16:22:04 -0600

    more seed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,6 +19,8 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+    undo_users()
+    undo_projects()
     seed_users()
     seed_projects()
     # Add other seed functions here
