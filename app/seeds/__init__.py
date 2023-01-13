3eb3f5016f84998628387fd1856865879698dffc
eric chai <no8cai@gmail.com>
2023-01-13 15:48:09 -0600

    project seeding eric

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .projects import seed_projects, undo_projects
+
 
 from app.models.db import db, environment, SCHEMA
 
@@ -18,11 +20,13 @@ def seed():
         # Make sure to add all your other model's undo functions below
         undo_users()
     seed_users()
+    seed_projects()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_projects()
     undo_users()
     # Add other undo functions here
