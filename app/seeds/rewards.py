7cc6bb1f1156e10b4edfd8ccdecff16a9d97e895
Kirin <kirinagcaoili@gmail.com>
2023-01-13 14:39:49 -0800

    fixed projectId in rewards seeds

--- app/seeds/rewards.py
+++ app/seeds/rewards.py
@@ -3,8 +3,8 @@ from app.models import db, Reward, environment, SCHEMA
 def seed_rewards():
     
     reward1 = Reward(title="Reward 1 For Project 1", price="1.00", description="Includes a reward. Nice!", projectId=1)
-    reward2 = Reward(title="Reward 2 For Project 2", price="2.00", description="Includes a different reward. Wow!", projectId=1)
-    reward3 = Reward(title="Reward 3 For Project 3", price="3.00", description="This reward is just feel-good fuzzies. Whoa!", projectId=1)
+    reward2 = Reward(title="Reward 2 For Project 2", price="2.00", description="Includes a different reward. Wow!", projectId=2)
+    reward3 = Reward(title="Reward 3 For Project 3", price="3.00", description="This reward is just feel-good fuzzies. Whoa!", projectId=3)
     
     db.session.add(reward1)
     db.session.add(reward2)
