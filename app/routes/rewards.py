3fa1b3b7cc21b36de2a25553d3200a931d7adc7f
Kirin <kirinagcaoili@gmail.com>
2023-01-13 21:47:20 -0800

    routes for update and delete rewards

--- app/routes/rewards.py
+++ app/routes/rewards.py
@@ -52,3 +52,66 @@ def create_reward(id):
             "message": "Forbidden",
             "statusCode": 403
         }, 403
+        
+# UPDATE A REWARD
+@bp.route('/rewards/<int:id>', methods=["PUT"])
+def update_reward(id):
+    form = RewardForm()
+    reward = Reward.query.get(id)
+    
+    if not reward:
+        return {
+            "message": "Reward couldn't be found",
+            "statusCode": 404
+        }, 404
+    
+    project = Project.query.get(reward.projectId)
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if authenticate()['id'] == project.id:
+        if form.validate_on_submit():
+            form.populate_obj(reward)
+            db.session.add(reward)
+            db.session.commit()
+            return reward.to_dict_reward()
+            
+        if form.errors:
+            return {
+                "message": "Validation error",
+                "statusCode": 400,
+                "errors": form.errors
+            }, 400
+            
+    else:
+        return {
+            "message": "Forbidden",
+            "statusCode": 403
+        }, 403
+        
+# DELETE A REWARD
+@bp.route('/rewards/<int:id>', methods=["DELETE"])
+def delete_reward(id):
+    reward = Reward.query.get(id)
+    
+    if not reward:
+        return {
+            "message": "Reward couldn't be found",
+            "statusCode": 404
+        }, 404
+        
+    project = Project.query.get(reward.projectId)
+
+    if authenticate()['id'] == project.id:
+        db.session.delete(reward)
+        db.session.commit()
+        return {
+            "message": "Successfully deleted",
+            "statusCode": 200,
+            "id": id
+        }, 200
+        
+    else:
+        return {
+            "message": "Forbidden",
+            "statusCode": 403
+        }, 403
