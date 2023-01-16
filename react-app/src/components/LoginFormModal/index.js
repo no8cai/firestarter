710b0755a89ac18960df570c4b2c2f598299855a
kirin <kirinagcaoili@gmail.com>
2023-01-16 10:39:00 -0800

    signup and login forms set

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import * as sessionActions from "../../store/session";
+import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import "./LoginForm.css";
@@ -7,66 +7,66 @@ import { useHistory } from "react-router-dom";
 
 function LoginFormModal() {
   const dispatch = useDispatch();
-//   const [credential, setCredential] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [errors, setErrors] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
   const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // setErrors([]);
-    // return dispatch(sessionActions.login({ credential, password }))
-    //   .then(closeModal)
-    //   .then(history.push('/'))
-    //   .catch(
-    //     async (res) => {
-    //       const data = await res.json();
-    //       console.log(data.errors)
-    //       if (data && data.errors) setErrors(data.errors);
-    //     }
-    //   );
+    setErrors([]);
+    return dispatch(sessionActions.login({ email, password }))
+      .then(closeModal)
+      .then(history.push('/'))
+      .catch(
+        async (res) => {
+          const data = await res.json();
+          console.log(data.errors)
+          if (data && data.errors) setErrors(data.errors);
+        }
+      );
   };
 
-//   const demoLogin = (e) => {
-//     e.preventDefault();
-//     const demo = {
-//         credential: 'Demo-lition',
-//         password: 'password'
-//     }
-//     return dispatch(sessionActions.login(demo)).then(closeModal)
-//     .then(closeModal)
-//     .then(history.push('/'))
-//   }
+  const demoLogin = (e) => {
+    e.preventDefault();
+    const demo = {
+        email: 'demo@aa.io',
+        password: 'password'
+    }
+    return dispatch(sessionActions.login(demo)).then(closeModal)
+    .then(closeModal)
+    .then(history.push('/'))
+  }
 
   return (
     <div className='login-holder'>
       <div className='login-line-holder'>
 
-      <h1 className='login-line'>Log in</h1>
+      {/* <h1 className='login-line'>Log in</h1> */}
       </div>
 
-      {/* <div className='welcome'> <h3 className='weclome-h3'>Welcome to Sparebnb</h3></div> */}
+      <div className='welcome'> <h3 className='weclome-h3'>Log In</h3></div>
 
     <div className='form-holder'>
 
       <form className='login-form-css' onSubmit={handleSubmit}>
-        {/* <ul className='errorlist'>
+        <ul className='errorlist'>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
-        </ul> */}
+        </ul>
           <div className='input-holder'>
         <label>
           
           <input
           className='input-line'
             type="text"
-            // value={credential}
+            value={email}
             placeholder='Email'
             title='Email Address'
-            // onChange={(e) => setCredential(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
         </label>
@@ -75,10 +75,10 @@ function LoginFormModal() {
           <input
             type="password"
             className='input-line2'
-            // value={password}
+            value={password}
             placeholder='Password'
             title='Password'
-            // onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
         </label>
@@ -86,12 +86,12 @@ function LoginFormModal() {
         <button type="submit">Log In</button>
       </form>
 
-            {/* <div className='form-holder'>
+            <div className='form-holder'>
                <form className='login-form-css' onSubmit={demoLogin}>
                 <div className='login-break'>or</div>
       <button type="submit">Demo User Login</button>
     </form>
-            </div> */}
+            </div>
      
     </div>
     </div>
