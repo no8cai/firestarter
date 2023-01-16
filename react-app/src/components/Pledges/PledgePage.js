2b9a2dba351e9c3e91a7329b03826fa10ee98c25
cory-bogert <bogert.cory@gmail.com>
2023-01-16 16:01:40 -0600

    pulling from main redone store and main page

--- react-app/src/components/Pledges/PledgePage.js
+++ react-app/src/components/Pledges/PledgePage.js
@@ -18,10 +18,18 @@ const PledgeDetails = () => {
     // console.log('322323232', projectDetails)
 
     useEffect(() => {
-        dispatch(getOneProject(id))
+        // dispatch(getOneProject(id))
         dispatch(getAllRewards(id))
         // dispatch(getAllPledges())
     }, [dispatch])
+
+    let rewards = useSelector(state => state.rewards)
+    console.log('rewards------', rewards)
+    let rewardsArr = Object.values(rewards)
+    console.log('rewardsArr-----', rewardsArr)
+    if(!rewardsArr) return null
+
+
 
 
     // let pledges = useSelector(state => {return state.pledges})
@@ -36,7 +44,7 @@ const PledgeDetails = () => {
                 <button>KICKSTARTER</button>
             </div> */}
             <div className='pledge-project-title'>
-                <h1>Title</h1>
+                {/* <h1>Title</h1> */}
                 <h1>Title</h1>
                 <h5>creator</h5>
             </div>
@@ -51,6 +59,35 @@ const PledgeDetails = () => {
             <div className='reward-selection-text'>
                 <h2>Select your reward</h2>
                 <p>Select an option below</p>
+            </div>
+
+
+
+                <input type='radio' />Pledge $20
+
+
+            <div className='reward-container'>
+                {rewardsArr.map(reward => {
+                    return (
+                        <div className='reward-card'>
+
+                        <div className='reward-card-details'>
+                            <h4 className='reward-price'>${reward.price}</h4>
+                            <h4 className='reward-title'>${reward.title}</h4>
+                            <p className='reward-description'>{reward.description}</p>
+                        </div>
+                        <div className='reward-card-shipping'>
+                            <h6 className='reward-estimated'>ESTIMATED DELIVERY</h6>
+                            <h5 className='reward-estimated'>{reward.estimatedDelivery}</h5>
+                            <h6 className='reward-estimated'>SHIPS TO</h6>
+                            <h5 className='reward-estimated'>Anywhere in the world</h5>
+                        </div>
+
+                        </div>
+
+                    )
+                })}
+
             </div>
 
 
