5c06a8b2a8b79b89775c81c4e957d2102e2caad5
cory-bogert <bogert.cory@gmail.com>
2023-01-16 09:24:22 -0600

    pledge page started

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import PledgeDetails from './components/PledgePage';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -31,6 +32,9 @@ function App() {
         <Route path='/login' exact={true}>
           <LoginForm />
         </Route>
+        <Route exact path='/'>
+            <PledgeDetails />
+          </Route>
         <Route path='/sign-up' exact={true}>
           <SignUpForm />
         </Route>
---
+++ react-app/src/components/PledgePage.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllPledges, deletePledge, updatePledge } from '../store/pledge';
+
+
+const PledgeDetails = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getAllPledges())
+    }, [dispatch])
+
+    let pledges = useSelector(state => state.pledges)
+    let pledgesArr = Object.values(pledges)
+    console.log('--------all the pledges---------', pledgesArr)
+    if(!pledgesArr) return null
+
+    return(
+        <>
+            <div className='pledge-outter-container'>
+            {pledgesArr.map(pledge => {
+                return (
+                    <h1>{pledge.backerId}</h1>
+                )
+            })}
+
+
+
+            </div>
+        </>
+    )
+}
+export default PledgeDetails
