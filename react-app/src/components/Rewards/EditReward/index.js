aebc03e3afda70faf74454ae96ed0d2c0df1ceea
kirin <kirinagcaoili@gmail.com>
2023-01-19 16:31:32 -0800

    merge conflicts

--- react-app/src/components/Rewards/EditReward/index.js
+++ react-app/src/components/Rewards/EditReward/index.js
@@ -1,12 +1,12 @@
-import React from "react";
+// import React from "react";
+import React, { useEffect, useState } from 'react'
 import { fetchCreateReward } from "../../../store/reward";
 import { fetchAllProjects } from "../../../store/project";
 import { fetchProjectRewards } from '../../../store/reward';
 import RewardForm from "../RewardForm";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import React, { useEffect, useState } from 'react'
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 // import React from "react"
 
 const EditReward=()=>{
