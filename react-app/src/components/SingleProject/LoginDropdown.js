9d0127041d219272d525d61123e7ca9bda6fdca5
amcpeek <ammcpeek@gmail.com>
2023-02-18 15:03:39 -0800

    drop down to log in on the single project page green button dropping down directly below the button

--- react-app/src/components/SingleProject/LoginDropdown.js
+++ react-app/src/components/SingleProject/LoginDropdown.js
@@ -14,63 +14,63 @@ function LoginDropdown({ user }) {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
     const ulRef = useRef();
-  
+
     const openMenu = () => {
       if (showMenu) return;
       setShowMenu(true);
     };
-  
+
     useEffect(() => {
       if (!showMenu) return;
-  
+
       const closeMenu = (e) => {
         if (!ulRef.current.contains(e.target)) {
           setShowMenu(false);
         }
       };
-  
+
       document.addEventListener('click', closeMenu);
-  
+
       return () => document.removeEventListener("click", closeMenu);
     }, [showMenu]);
-  
+
     const closeMenu = () => setShowMenu(false);
-  
+
     // const logout = (e) => {
     //   e.preventDefault();
     //   dispatch(sessionActions.logout());
     //   closeMenu();
     //   history.push('/')
     // };
-  
+
     // let pClassName = "profile-button-outline"
     let ulClassName = "sp-dropdown" + (showMenu ? "" : " hidden");
     // if (!user) {
     //  let ulClassName = "profile-dropdown2" + (showMenu ? "" : " hidden")
-    //   pClassName = "profile-button" 
+    //   pClassName = "profile-button"
     // }
-  
+
     return (
-      <div 
+      <div
       // className='profile-button-div'
       >
-        <button 
-        // style={{ backgroundImage: `url('${proficon}'`, backgroundSize: 'contain' }} 
-        onClick={openMenu} 
+        <button
+        // style={{ backgroundImage: `url('${proficon}'`, backgroundSize: 'contain' }}
+        onClick={openMenu}
         className="sp-green-button"
-        >Log in or sign up to back this project!</button>
+        >Log in to back this project</button>
         <div className={ulClassName} ref={ulRef}>
-          
+
               <div className='signup-button'>
             <div className='login-button'>
               <OpenModalMenuItem
                 itemText="Log In"
                 onItemClick={closeMenu}
-                modalComponent={<LoginFormModal 
+                modalComponent={<LoginFormModal
                 />}
               />
             </div>
-              
+
               <div ></div>
               <OpenModalMenuItem
                 itemText="Sign Up"
@@ -82,5 +82,5 @@ function LoginDropdown({ user }) {
       </div>
     );
   }
-  
+
   export default LoginDropdown;
